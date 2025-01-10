5f89bdc9d5daf9c177c9daa9d8861aec300f00cc
macasubo <macasubo@student.42.fr>
2025-01-10 18:05:29 +0100

    ex04 done

--- module00/ex04/whatis.py
+++ module00/ex04/whatis.py
@@ -1,11 +1,12 @@
 import sys
 
 if len(sys.argv) == 1:
-	print()
 	exit()
+
 try:
 	assert len(sys.argv) == 2, "more than one argument is provided"
-	assert sys.argv[1].isdigit(), "argument is not an integer"
 	print("I'm Even." if int(sys.argv[1]) % 2 == 0 else "I'm Odd.")
 except AssertionError as error:
 	print(error)
+except ValueError:
+	print("AssertionError: argument is not an integer")
